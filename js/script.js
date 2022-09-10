526c81cacf1f376dae3f67f96d5e83394b4a98f0
Paulo <trigopaulo@gmail.com>
2022-09-10 14:47:21 -0300

    javascript

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Initial References
+
+let decInp = document.getElementById("dec-input");
+let binInp = document.getElementById("bin-input");
+
+let errorMsg = document.getElementById("error-msg");
